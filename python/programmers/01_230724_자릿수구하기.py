ec08c1b9bb224b6a7dd8251048abf71f2bfaf974
jooeunho <pureredgrace@gmail.com>
2023-07-24 20:44:15 +0900

    자릿수 구하기 다른사람 풀이 추가

--- python/programmers/01_230724_자릿수구하기.py
+++ python/programmers/01_230724_자릿수구하기.py
@@ -7,4 +7,17 @@ def solution(n):
 
     return answer
 
+#  재귀함수를 사용한 풀이
+def sum_digit(number):
+    if number < 10:
+        return number
+
+    # number가 10 미만이 될때까지 재귀 실행
+    return number%10 + sum_digit(number//10)
+
+
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print("결과 : {}".format(sum_digit(123)));
+
 print(solution(987))
